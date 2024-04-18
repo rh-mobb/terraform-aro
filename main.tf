e7795551cec280a3575be922ac911e2f99119bba
Dustin Scott <dscott@redhat.com>
2024-04-18 16:18:56 -0500

    feat: correct indexing of api/ingress profile blocks
    Signed-off-by: Dustin Scott <dscott@redhat.com>

--- main.tf
+++ main.tf
@@ -145,9 +145,9 @@ output "console_url" {
 }
 
 output "api_servier_ip" {
-  value = azurerm_redhat_openshift_cluster.cluster.api_server_profile.ip_address
+  value = azurerm_redhat_openshift_cluster.cluster.api_server_profile[0].ip_address
 }
 
 output "ingress_ip" {
-  value = azurerm_redhat_openshift_cluster.cluster.ingress_profile.ip_address
+  value = azurerm_redhat_openshift_cluster.cluster.ingress_profile[0].ip_address
 }
