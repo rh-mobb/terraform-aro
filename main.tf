ddde4be1f459630d7db80e9e7f445f8885f2640b
Paul Czarkowski <username.taken@gmail.com>
2024-09-25 13:41:48 -0400

    update cp subnet privatelink service network policies
    Signed-off-by: Paul Czarkowski <username.taken@gmail.com>

--- main.tf
+++ main.tf
@@ -25,10 +25,10 @@ resource "azurerm_subnet" "control_plane_subnet" {
   virtual_network_name                           = azurerm_virtual_network.main.name
   address_prefixes                               = [var.aro_control_subnet_cidr_block]
   service_endpoints                              = ["Microsoft.Storage", "Microsoft.ContainerRegistry"]
-  private_link_service_network_policies_enabled = true
+  private_link_service_network_policies_enabled = false
   # enforce_private_link_service_network_policies  = true
   # private_endpoint_network_policies_enabled = true
-  private_endpoint_network_policies = "Disabled"
+  # private_endpoint_network_policies = "Disabled"
   # enforce_private_link_endpoint_network_policies = true
 
 }
